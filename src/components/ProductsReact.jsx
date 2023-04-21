6c0ed2ee0a3849522dd0401caef0e0af5fc12437
Valentino Geronzi <valentinogeronzi@hotmail.com>
2023-04-21 11:46:59 -0300

    "Nosotros" section added, and other css changes

--- src/components/ProductsReact.jsx
+++ src/components/ProductsReact.jsx
@@ -1,24 +1,58 @@
 
 import React, { useState, useEffect } from 'react';
-/* import { Orbit } from '@uiball/loaders'; */
 import data from "../../products.json"
 import { motion, AnimatePresence } from "framer-motion"
+import { Orbit } from '@uiball/loaders'
 
 export default function Products() {
 
     const [search, setSearch] = useState("");
     const [products, setProducts] = useState([]);
-    const [itemsQuantity, setItemsQuantity] = useState(8);
+    const [itemsQuantity, setItemsQuantity] = useState(
+        JSON.parse(localStorage.getItem('itemsQuantity') || 8));
     const [visibility, setVisibility] = useState(true);
+    const [loading, setLoading] = useState(false);
 
-    let todo = data;
-    let filtered = products;
-    let buscado = "";
+
+    const fetchData = async () => {
+        const response = await fetch('../../products.json');
+        const json = await response.json();
+        setProducts(json);
+        setLoading(true);
+    };
 
     useEffect(() => {
-        setProducts(data)
+        fetchData()
     }, [])
 
+    /* useEffect to make "show more" btn dissapear while there is no more products to show */
+    let filtered = [];
+    useEffect(() => {
+        filtered = products.filter(val => val.name.toLowerCase().includes(search.toLowerCase()));
+
+        if (itemsQuantity >= filtered.length) {
+            setVisibility(false);
+        } else if (filtered.length <= 8 && filtered.length >= 0) {
+            setVisibility(false);
+        } else {
+            setVisibility(true);
+        }
+
+    }, [search, itemsQuantity, products])
+
+    /* useEffect to make "show more" btn dissapear after 30s */
+    useEffect(() => {
+        localStorage.setItem('itemsQuantity', itemsQuantity);
+
+        const timeout = setTimeout(() => {
+            setItemsQuantity(8);
+        }, 30000);
+
+        return () => clearTimeout(timeout);
+
+    }, [itemsQuantity])
+
+    /* category sorting btn handler */
     const sortCategory = (category) => {
         let inCategory = data.filter(prod => prod.category === category)
         setVisibility(false)
@@ -26,29 +60,15 @@ export default function Products() {
         setItemsQuantity(inCategory.length)
     }
 
+
     return (<>
         <div className='product-container'>
-            <h2 className="h2_products" >Productos</h2>
+            <h2 className="h2_products" id="products">Productos</h2>
             <div className="search_bar_container">
                 <div className="searchBar">
 
                     <input onChange={(e) => {
                         setSearch(e.target.value)
-                        buscado = e.target.value
-
-                        filtered = products.filter(val => val.name.toLowerCase().includes(buscado.toLowerCase()))
-                            .slice(0, itemsQuantity)
-
-                        if (itemsQuantity >= products.length - 8) {
-                            setVisibility(false);
-                        }
-                        else if (filtered.length < 8 && filtered.length >= 0) {
-                            setVisibility(false);
-                        }
-                        else {
-                            setVisibility(true);
-                        }
-
                     }} id="searchQueryInput" type="text" name="searchQueryInput" placeholder="Buscar" />
 
                     <button id="searchQuerySubmit" type="submit" name="searchQuerySubmit">
@@ -61,7 +81,7 @@ export default function Products() {
             {/* categorias categorias categorias*/}
             <div className="categories-container">
                 <button className='category-btn' onClick={() => {
-                    setProducts(todo)
+                    setProducts(data)
                     setVisibility(true)
                     setItemsQuantity(8)
                 }
@@ -94,60 +114,79 @@ export default function Products() {
                 </button>
             </div>
 
-            <motion.ul className="link-card-grid">
-                {
-                    products
-                        .filter(val => val.name.toLowerCase().includes(search.toLowerCase()))
-                        .slice(0, itemsQuantity)
-                        .map((val, id) => (
-                            <AnimatePresence key={id}>
-                                <motion.li layout
-                                    transition={{
+            {
+                loading ?
+                    <>
+                        <motion.ul className="link-card-grid">
+                            {
+                                products
+                                    .filter(val => val.name.toLowerCase().includes(search.toLowerCase()))
+                                    .slice(0, itemsQuantity)
+                                    .map((val, id) => (
+                                        <AnimatePresence key={id}>
+                                            <motion.li layout
+                                                transition={{
 
-                                        opacity: {
-                                            duration: 2,
-                                            ease: [.12, .98, .67, .96]
-                                        },
-                                        y: {
-                                            duration: 1,
-                                            ease: [.12, .98, .67, .96]
-                                        }
-                                    }}
-                                    whileInView={{ opacity: 1, y: 70 }}
-                                    key={val.id} className="link-card">
+                                                    opacity: {
+                                                        duration: 2,
+                                                        ease: [.12, .98, .67, .96]
+                                                    },
+                                                    y: {
+                                                        duration: 1,
+                                                        ease: [.12, .98, .67, .96]
+                                                    }
+                                                }}
+                                                whileInView={{ opacity: 1, y: 70 }}
+                                                key={val.id}
+                                                className="link-card">
 
 
-                                    <a href={`/product-details/${val.id}`}>
-                                        <h3>
-                                            {val.name}
-                                        </h3>
-                                    </a>
-                                    <a href={`/product-details/${val.id}`}>
-                                        <img src={val.src} alt="Product Image" />
-                                    </a>
-                                    <p>
-                                        {val.description}
-                                    </p>
-                                </motion.li>
-                            </AnimatePresence>
-                        )
-                        )
-                }
-            </motion.ul>
+                                                <a href={`/product-details/${val.id}`}>
+                                                    <h3>
+                                                        {val.name}
+                                                    </h3>
+                                                </a>
+                                                <a href={`/product-details/${val.id}`}>
+                                                    <img src={val.src} alt="Product Image" />
+                                                </a>
+                                                <p>
+                                                    {val.description}
+                                                </p>
+                                            </motion.li>
+                                        </AnimatePresence>
+                                    )
+                                    )
+                            }
+                        </motion.ul>
 
-            {visibility ?
-                <button onClick={() => {
-                    setItemsQuantity(itemsQuantity + 8)
-                    if (itemsQuantity >= products.length - 8) {
-                        setVisibility(false);
-                    }
-                }}
-                    className='show-more'>
-                    <span className="text">Ver Mas</span>
-                    <span>&#8595;</span>
-                </button>
-                :
-                <></>}
+                        {visibility ?
+                            <button onClick={() => {
+                                setItemsQuantity(itemsQuantity + 8)
+
+                                if (itemsQuantity >= filtered.length - 8) {
+                                    setVisibility(false);
+                                }
+                            }}
+                                className='show-more'>
+                                <span className="text">Ver Mas</span>
+                                <span>&#8595;</span>
+                            </button>
+                            :
+                            <></>}
+                    </>
+
+                    :
+
+                    /* Loader */
+                    <div className="loader">
+                        <Orbit
+                            size={35}
+                            speed={1.5}
+                            color="white"
+                        /></div>
+
+            }
+
 
 
         </div>
