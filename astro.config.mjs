0d37ba450514a554c02853f58963ddc1efb4cddf
Valentino Geronzi <valentinogeronzi@hotmail.com>
2023-05-22 15:46:09 -0300

    github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 import react from "@astrojs/react";
 
-
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
-})
+   integrations: [react()],
+   site: "https://valenmetal.github.io",
+   base: "/React-Astro-E-Commerce",
+});
