4eb485576a4e49e5355df36c0fd18a9478fdfeac
Valentino Geronzi <valentinogeronzi@hotmail.com>
2023-05-29 15:41:22 -0300

    Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,13 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 
 // https://astro.build/config
+import vercel from "@astrojs/vercel/serverless";
+
+// https://astro.build/config
 export default defineConfig({
-   integrations: [react()],
-   site: "https://valenmetal.github.io",
-   base: "/React-Astro-E-Commerce",
+  integrations: [react()],
+  site: "https://valenmetal.github.io",
+  base: "/React-Astro-E-Commerce",
+  output: "server",
+  adapter: vercel()
 });
