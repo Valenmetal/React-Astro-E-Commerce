67d8cda34cd0e59db5d8bc8127373bd83e21ca92
Valentino Geronzi <valentinogeronzi@hotmail.com>
2023-05-29 16:22:45 -0300

    Updates

---
+++ entry.mjs
@@ -0,0 +1,10 @@
+export { renderers } from './renderers.mjs';
+import './chunks/astro.d097d118.mjs';
+import 'html-escaper';
+import 'react';
+import 'react-dom/server';
+
+const _page0 = () => import('./chunks/pages/index.astro.b47d087e.mjs');
+const _page1 = () => import('./chunks/pages/_id_.astro.a0d9d23b.mjs').then(n => n._);const pageMap = new Map([["src/pages/index.astro", _page0],["src/pages/product-details/[id].astro", _page1],]);
+
+export { pageMap };
